fbbc695311bdb92ebb3a87ead104ed5dd7cc31e8
Elen <elcrses@ya.ru>
2019-11-19 21:43:09 +0300

    CountdownPart.js: add a function to format input value

--- components/CountdownPart.js
+++ components/CountdownPart.js
@@ -1,8 +1,14 @@
 function CountdownPart(props) {
+
+    const formattedNumber = number => {
+        const prefix = number < 10 ? '0' : '';
+        return prefix + number.toString();
+    }
+
     return (
         <div className={props.partClassName}>
             <div>
-                {props.value}
+                {formattedNumber(props.value)}
             </div>
             <div>
                 {props.valueText}
