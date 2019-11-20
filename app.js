5c32f08c9797de159786d83e818d5e43c0e56bc0
Elen <elcrses@ya.ru>
2019-11-20 16:02:08 +0300

    app.js: add initial values to the form controls and to the component

--- app.js
+++ app.js
@@ -9,20 +9,33 @@ document.querySelector('button').addEventListener('click', () => {
     const text = document.querySelector("input[type='text']").value;
     const date = document.querySelector("input[type='date']").value;
     const time = document.querySelector("input[type='time']").value;
+    const timeZone = ''; //document.querySelector("input.timeZone").value;
 
-    const utcString = new Date(`${date} ${time}`).toUTCString();
-    console.log(utcString);
+    const utcString = new Date(`${date} ${time}${timeZone}`).toUTCString();
+    console.log(date, time, utcString);
 
     ReactDOM.render(
-        <App text={text} date={utcString}/>, 
+        <Countdown 
+            text={text} 
+            date={utcString}
+        />, 
         document.getElementById('countdown-wrapper')
     );
 });
 
-ReactDOM.render(
-    <App 
-        text={"Time left"}
-        date={new Date().toUTCString()}    
-    />, 
-    document.getElementById('countdown-wrapper')
-);
+const initialValues = (text, dateString, timeString, timeZoneString) => {
+    document.querySelector("input[type='text']").value = text;
+    document.querySelector("input[type='date']").value = dateString;
+    document.querySelector("input[type='time']").value = timeString;
+
+    const utcString = new Date(`${dateString} ${timeString}${timeZoneString}`).toUTCString();
+    ReactDOM.render(
+        <Countdown 
+            text={text}
+            date={utcString}    
+        />, 
+        document.getElementById('countdown-wrapper')
+    );
+}
+
+initialValues('Advent Of Code will start at', '2019-12-01', '00:00', '-05:00');
