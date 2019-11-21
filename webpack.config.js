6fa63faf1faf925870bf2fe1bc7065f5f7739b89
Elen <elcrses@ya.ru>
2019-11-21 23:31:54 +0300

    webpack.config.js: add exclude node_modules to the rules

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use: 'babel-loader'
+                use: 'babel-loader',
+                exclude: /node_modules/
             }
         ]
     },
